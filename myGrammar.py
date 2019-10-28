25355223a0cbe86d87d7cbdeda6d43672180f0ba
RodrigoDestefano <rodrigo.destefano.santos@gmail.com>
2019-10-28 14:00:03 -0300

    Add definicao de operadores relacionais

--- myGrammar.py
+++ myGrammar.py
@@ -6,12 +6,14 @@ from myLexer import tokens
 precedence = (
     ('left','SUM','SUBTRACTION'),
     ('left','MULTIPLICATION','DIVISION'),
+    ('nonassoc', 'GREATER', 'GREATEROREQUAL', 'LESS', 'LESSOREQUAL', 'EQUAL', 'DIFFERENT'),
     #('right','UMINUS'),
     )
 
 variables = { }
 
-# Atribui o resultado de uma expressão na variavel
+### Atribui o resultado de uma expressão na variavel
+
 def p_statement_assign(t):
     'statement : VARIABLE ASSINGMENT expression'
     variables[t[1]] = t[3]
@@ -39,13 +41,18 @@ def p_expression_group(t):
     'expression : OPEN_PARENTHESIS expression CLOSE_PARENTHESIS'
     t[0] = t[2]
 
+### Define expressao de entrada e saida
+
 def p_expression_scan(t):
     'expression : SCAN OPEN_PARENTHESIS VARIABLE CLOSE_PARENTHESIS'
     variables[t[3]] = input()
 
 def p_expression_print(t):
-    'expression : PRINT OPEN_PARENTHESIS VARIABLE CLOSE_PARENTHESIS'
-    print(variables[t[3]])
+    'expression : PRINT OPEN_PARENTHESIS expression CLOSE_PARENTHESIS'
+    print(t[3])
+
+
+### Define expressao de tipos de variaveis
 
 def p_expression_int(t):
     'expression : INT VARIABLE ASSINGMENT DIGIT'
@@ -56,12 +63,14 @@ def p_expression_float(t):
     variables[t[2]] = float(t[4])
 
 def p_expression_string(t):
-    'expression : STRING VARIABLE ASSINGMENT QUOTATION_MARKS VARIABLE QUOTATION_MARKS'
-    variables[t[2]] = str(t[5])
+    'expression : STRING VARIABLE ASSINGMENT VARIABLE'
+    variables[t[2]] = str(t[4])
 
 def p_expression_char(t):
-    'expression : CHAR VARIABLE ASSINGMENT SINGLE_QUOTATION_MARK VARIABLE SINGLE_QUOTATION_MARK'
-    variables[t[2]] = t[5]
+    'expression : CHAR VARIABLE ASSINGMENT VARIABLE'
+    variables[t[2]] = t[4]
+
+### Define expressao de variaveis
 
 def p_expression_digit(t):
     'expression : DIGIT'
@@ -78,6 +87,34 @@ def p_expression_variable(t):
     except LookupError:
         print("Undefined name '%s'" % t[1])
         t[0] = 0
+
+### Define expressao de operadores relacionais
+
+def p_expression_greater(t):
+    'expression : expression GREATER expression'
+    t[0] = (t[1] > t[3])
+
+def p_expression_greater_equal(t):
+    'expression : expression GREATEROREQUAL expression'
+    t[0] = (t[1] >= t[3])
+
+def p_expression_less(t):
+    'expression : expression LESS expression'
+    t[0] = (t[1] < t[3])
+
+def p_expression_less_equal(t):
+    'expression : expression LESSOREQUAL expression'
+    t[0] = (t[1] <= t[3])
+
+def p_expression_equal(t):
+    'expression : expression EQUAL expression'
+    t[0] = (t[1] == t[3])   
+
+def p_expression_different(t):
+    'expression : expression DIFFERENT expression'
+    t[0] = (t[1] != t[3])  
+
+### Define expressao de erro
 
 def p_error(t):
     print("Syntax error at '%s'" % t.value)
